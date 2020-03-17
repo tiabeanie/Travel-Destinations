98093ed7bfd3876908802ba6e29b89c8fc388aa3
Sydney_Hunter <sydney.jane.hunter@gmail.com>
2020-03-17 12:38:05 -0600

    updated destinations controller

--- travel/app/controllers/destinations_controller.rb
+++ travel/app/controllers/destinations_controller.rb
@@ -1,15 +1,15 @@
-class ExperiencesController < ApplicationController
-    before '/experiences/*' do
+class destinationsController < ApplicationController
+    before '/destinations/*' do
       if !is_logged_in?
         flash[:login] = "You need to be logged in for that!"
         redirect to '/login'
       end
     end
   
-    get '/experiences' do
+    get '/destinations' do
       if is_logged_in?
         @user = current_user
-        erb :"experiences/experiences"
+        erb :"destinations/destinations"
       else
         flash[:login] = "You need to be logged in for that!"
         redirect to '/login'
@@ -61,3 +61,55 @@ class ExperiencesController < ApplicationController
   
       @destination = Destination.create_new_destination(details, country_name, country_id, session[:user_id])
   
+      flash[:success] = "You added a new destination!!"
+      redirect to "destinations/#{@destination.id}"
+    end
+  
+    get '/destinations/:id/edit' do
+      @user = current_user
+      @destination = Destination.find(params["id"])
+      if @user.id != @destination.user_id
+        flash[:edit_user] = "You can only edit your own destinations"
+        redirect to "/destinations/#{@destination.id}"
+      end
+      erb :"destinations/edit"
+    end
+  
+    patch '/destinations/:id' do
+      destination = Destination.find(params[:id])
+  
+      details = {
+        :description => params["description"],
+        :country => params["country"]
+      }
+      category_name = params["category"]["name"]
+      category_ids = params["category"]["category_ids"]
+  
+      is_empty?(details, "destinations/#{params[:id]}/edit")
+  
+      exp = Destination.update_destination(details, category_name, category_ids, destination)
+  
+      flash[:success] = "Your destination has been updated!"
+      redirect to "destinations/#{exp.id}"
+    end
+  
+    delete '/destinations/:id/delete' do
+      @user = current_user
+      @destination = Destination.find(params[:id])
+      if @user.id != @destination.user_id
+        flash[:edit_user]
+        redirect to '/destinations/#{@destination.id}'
+      else
+        @destination.destroy
+        flash[:deleted] = "Your destination has been deleted"
+        redirect to '/destinations'
+      end
+    end
+  
+    get '/destinations/:id' do
+      @user = current_user
+      @destination = Destination.find(params["id"])
+      erb :"destinations/show"
+    end
+  
+  end
