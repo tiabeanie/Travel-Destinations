f14e0fd50c1522e5282b19d80702ab808a947d09
Sydney_Hunter <sydney.jane.hunter@gmail.com>
2020-03-17 12:39:44 -0600

    added countries controller

---
+++ travel/app/controllers/countries_controller.rb
@@ -0,0 +1,22 @@
+class CountriesController < ApplicationController
+    before '/countries/*' do
+      if !is_logged_in?
+        flash[:login] = "You need to be logged in for that!"
+        redirect to '/login'
+      end
+    end
+  
+    get '/countries' do
+      if !is_logged_in?
+        flash[:login] = "You need to be logged in for that!"
+        redirect to '/login'
+      end
+      @countries = Country.all.sort_by{|c| c.name}
+      erb :"countries/countries"
+    end
+  
+    get '/countries/:id' do
+      @country = Country.find(params["id"])
+      erb :"countries/show"
+    end
+  end
