68eb1ba9353c446f63e4fc37baf2f246c3f74a74
Sydney_Hunter <sydney.jane.hunter@gmail.com>
2020-03-20 11:59:26 -0600

    updated controllers

--- travel/app/controllers/application_controller.rb
+++ travel/app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+    set :sesion_secret, 'lovetotravel'
   end
 
 
--- travel/app/controllers/destinations_controller.rb
+++ travel/app/controllers/destinations_controller.rb
@@ -1,111 +1,45 @@
-class DestinationsController < ApplicationController
-    before '/destinations/*' do
-      if !is_logged_in?
-        flash[:login] = "You need to be logged in for that!"
-        redirect to '/login'
-      end
-    end
-  
-    get '/destinations' do
-      if is_logged_in?
-        @user = current_user
-        erb :"destinations/destinations"
-      else
-        flash[:login] = "You need to be logged in for that!"
-        redirect to '/login'
-      end
-    end
-  
-    get '/destinations/users' do
-      @user = current_user
-      erb :"destinations/destinations_from_user"
-    end
-  
-  
-    post '/destinations' do
-      details = {
-        :description => @params["description"],
-        :country => @params["country"]
-      }
-  
-      is_empty?(details, 'destinations/new')
-  
-      @destination = Destination.create_new_destinaion(details, country_name, country_id, session[:user_id])
-  
-      flash[:success] = "You created a new destination!"
-      redirect to "destinations/#{@destination.id}"
-    end
-  
-    get '/destinations/:id/new_from_user' do
-      user = current_user
-      @destination = Destination.find(params["id"])
-      if @destination.user_id == user.id
-        flash[:add_from_user] = "You already added this destination!"
-        redirect to "/destinations/#{params["id"]}"
-      else
-        erb :"destinations/create_from_user"
-      end
-    end
-  
-    post '/destinations/new_from_user' do
-      details = {
-        :description => @params["description"],
-        :country => @params["country"]
-      }
-  
-      is_empty?(details, "destinations/#{params[:id]}/new_from_user")
-  
-      @destination = Destination.create_new_destination(details, country_name, country_id, session[:user_id])
-  
-      flash[:success] = "You added a new destination!!"
-      redirect to "destinations/#{@destination.id}"
-    end
-  
-    get '/destinations/:id/edit' do
-      @user = current_user
-      @destination = Destination.find(params["id"])
-      if @user.id != @destination.user_id
-        flash[:edit_user] = "You can only edit your own destinations"
-        redirect to "/destinations/#{@destination.id}"
-      end
-      erb :"destinations/edit"
-    end
-  
-    patch '/destinations/:id' do
-      destination = Destination.find(params[:id])
-  
-      details = {
-        :description => params["description"],
-        :country => params["country"]
-      }
-      category_name = params["category"]["name"]
-      category_ids = params["category"]["category_ids"]
-  
-      is_empty?(details, "destinations/#{params[:id]}/edit")
-  
-      exp = Destination.update_destination(details, category_name, category_ids, destination)
-  
-      flash[:success] = "Your destination has been updated!"
-      redirect to "destinations/#{exp.id}"
-    end
-  
-    delete '/destinations/:id/delete' do
-      @user = current_user
-      @destination = Destination.find(params[:id])
-      if @user.id != @destination.user_id
-        flash[:edit_user]
-        redirect to '/destinations/#{@destination.id}'
-      else
-        @destination.destroy
-        flash[:deleted] = "Your destination has been deleted"
-        redirect to '/destinations'
-      end
-    end
-  
-    get '/destinations/:id' do
-      @user = current_user
-      @destination = Destination.find(params["id"])
-      erb :"destinations/show"
-    end
-  
+class DestinationsController < ApplicationController 
+  get "/destinations" do
+    redirect_if_not_logged_in 
+    @destination = Destination.all
+    erb :'destinations/index'
   end
+
+  get "/destinations/new" do
+    redirect_if_not_logged_in 
+    @error_message = params[:error]
+    erb :'destinations/new'
+  end
+
+  get "/destinations/:id/edit" do
+    redirect_if_not_logged_in 
+    @error_message = params[:error]
+    @destination = Destination.find(params[:id])
+    erb :'destinations/edit'
+  end
+
+  post "/destinations/:id" do
+    redirect_if_not_logged_in 
+    @destination = Destination.find(params[:id])
+    unless Destination.valid_params?(params)
+      redirect "/destinations/#{@destination.id}/edit?error=invalid destination"
+    end
+    @destination.update(params.select{|k|k=="name" || k=="country_id"})
+    redirect "/destinations/#{@destination.id}"
+  end
+
+  get "/destinations/:id" do
+    redirect_if_not_logged_in 
+    @destination = Destination.find(params[:id])
+    erb :'destinations/show'
+  end
+
+  post "/destinations" do
+    redirect_if_not_logged_in 
+    unless Destination.valid_params?(params)
+      redirect "/destinations/new?error=invalid destination"
+    end
+    Destination.create(params)
+    redirect "/destinations"
+  end
+end
